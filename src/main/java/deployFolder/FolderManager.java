f314d30091ca31027ba166997e0d00d5e3ea6172
Dylan Duan <dylan.duan@2gotech.com>
2018-03-23 15:55:26 +0800

    commit

--- src/main/java/deployFolder/FolderManager.java
+++ src/main/java/deployFolder/FolderManager.java
@@ -2,7 +2,6 @@ package deployFolder;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
